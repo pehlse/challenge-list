f6d59ba02295d426f85daafc72f6f3fc9d1f8826
Pedro Henrique <pedro97henrique@gmail.com>
2022-10-18 14:50:15 -0300

    chore: create header component

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header class="header-wrapper">
+      <img class="header-wrapper__image" src="@/assets/deloitte.svg" alt="Logo">
+      <div>
+        <h2 class="header-wrapper__title">Frontend challenge</h2>
+      </div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss"  scoped>
+$logo-width: 100px;
+
+.header-wrapper {
+  display: inline-flex;
+  background-color: var(--g-gray);
+  min-height: var(--g-header-height);
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  padding: var(--g-space-s);
+  text-align: right;
+
+  &__image {
+    width: $logo-width;
+  }
+
+  &__title {
+    font: var(--title-headline-small)
+  }
+}
+</style>
