54427235167ada557653bc969dca985ef48204b1
Pedro Henrique <pedro97henrique@gmail.com>
2022-10-18 14:49:45 -0300

    chore: initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from '@/components/Header/Header.vue'
+
+</script>
+
+<template>
+  <Header/>
+  <body>
+    <RouterView />
+  </body>
+
+
+</template>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+}
+</style>
---
+++ src/views/HomeView/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <p>text-sm-center</p>
+  </main>
+</template>
